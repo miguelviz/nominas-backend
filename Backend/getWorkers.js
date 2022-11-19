6cd44add93ea9083bc2a2e2a24e36a45a4b734e6
Miguel Aguayo <miguel.aguayo.developer@gmail.com>
2022-11-19 11:01:16 -0600

    Payroll calculation is added

--- Backend/getWorkers.js
+++ Backend/getWorkers.js
@@ -7,19 +7,56 @@ const GetWorkers = async({req,res})=>{
         })
     })
     if(conn){
-        let query = `SELECT names, first_sname, second_sname FROM users`;
+        let query = `
+            SELECT 
+                a.names, a.first_sname, a.second_sname, a.month_packages, 
+                b.name_user_type, b.hour_bonus
+            FROM users a 
+            LEFT JOIN user_types b ON a.user_type = b.id_type`;
         let list = await MYSQL.executeArrayQuery({conn,query}).catch(e=>{
             res.json({
                 error:"Error al consultar los usuarios."
             })
         })
         if(list){
+            list = getPayrollUsers({list});
             res.json({
                 result:{
                     list
                 }
             });
         }
+    }
+}
+const getPayrollUsers = ({list})=>{
+    const ServerOptions = require("../Utils/ServerOptions");
+    const {payroll} = ServerOptions;
+    const {salary_worker,package_bonus,hours_worked_per_month,isr_porcent,isr_extra_porcent,max_salary_extra} = payroll;
+    if(list.length>0){
+        let newList = list.map(el=>el);
+        newList.forEach(user => {
+            let salary = Math.round((salary_worker*hours_worked_per_month)*100)/100;
+            let user_hour_bonus = Math.round((user.hour_bonus*hours_worked_per_month)*100)/100;
+            let user_package_bonus = Math.round((package_bonus*user.month_packages)*100)/100;
+            let full_salary = Math.round((user_package_bonus + user_hour_bonus + salary)*100)/100;
+            let porcent = isr_porcent;
+            if(full_salary>=max_salary_extra){
+                porcent += isr_extra_porcent;
+            }
+            let one_porcent = Math.round((full_salary/100)*100)/100;
+            let isr_total = Math.round((porcent*one_porcent)*100)/100;
+            full_salary = full_salary;
+            user.salary = salary; 
+            user.hours_worked_per_month = hours_worked_per_month;
+            user.hour_bonus = user_hour_bonus;
+            user.package_bonus = user_package_bonus;
+            user.full_salary = full_salary - isr_total;
+            user.isr_porcent = porcent;
+            user.isr_total = isr_total;
+        });
+        return newList;
+    }else{
+        return [];
     }
 }
 module.exports = GetWorkers;
--- Utils/ServerOptions.js
+++ Utils/ServerOptions.js
@@ -1,6 +1,14 @@
 const DefaultConn = require("../Mysql/DefaultConn");
 const ServerOptions = {
     production:false,
-    selectedDatabase:DefaultConn.database
+    selectedDatabase:DefaultConn.database,
+    payroll:{
+        salary_worker:30,
+        package_bonus:5,
+        hours_worked_per_month:172,
+        isr_porcent:9,
+        isr_extra_porcent:3,
+        max_salary_extra:10000
+    }
 }
 module.exports = ServerOptions;
